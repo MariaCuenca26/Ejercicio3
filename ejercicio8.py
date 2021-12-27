f6bdf286bfa0543d4691ea36d018c58d19fcb739
MariaCuenca26 <86026443+MariaCuenca26@users.noreply.github.com>
2021-12-26 20:23:27 -0500

    Add files via upload

---
+++ ejercicio8.py
@@ -0,0 +1,12 @@
+#ejercicio con operadores aritmeticos (modulo, exponente,division entera)
+numero1 = int(input("Ingrese el primer numero numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+#Procesos 
+divisionEntera= numero1//numero2
+exponente = numero1**numero2
+modulo = numero1 % numero2
+#salida
+print("El resultado de la division entera es: " + str(divisionEntera))
+print("El resultado del exponente es:" + str (exponente))
+print("El resultado del modulo:" + str (modulo))
+
